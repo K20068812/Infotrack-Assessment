44fade862399b28d6f195e9431ccc49947886e78
Amman Kiani <amman.kiani@kcl.ac.uk>
2025-06-25 22:54:04 +0100

    Add ef

---
+++ LandRegistryApi.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using LandRegistryApi.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LandRegistryApi.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SearchResult>(entity =>
+            {
+                entity.HasKey(sr => sr.Id);
+                entity.Property(sr => sr.SearchQuery).IsRequired().HasMaxLength(500);
+                entity.Property(sr => sr.TargetUrl).IsRequired().HasMaxLength(500);
+                entity.Property(sr => sr.Positions).IsRequired().HasMaxLength(1000);
+
+                // Index for faster queries on target URL and search date
+                entity.HasIndex(sr => new { sr.TargetUrl, sr.SearchDate });
+            });
+        }
+
+        public DbSet<SearchResult> SearchResults { get; set; }
+    }
+}
---
+++ LandRegistryApi.Infrastructure/Repositories/SearchResultRepository.cs
@@ -0,0 +1,33 @@
+﻿using LandRegistryApi.Core.Entities;
+using LandRegistryApi.Core.Interfaces;
+using LandRegistryApi.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LandRegistryApi.Infrastructure.Repositories
+{
+    public class SearchResultRepository : ISearchResultRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public SearchResultRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SearchResult>> GetAllSearchResultsAsync(string targetUrl, int days)
+        {
+            var cutOffDate = DateTime.UtcNow.AddDays(-days);
+
+            return await _context.SearchResults
+                .Where(sr => sr.TargetUrl == targetUrl && sr.SearchDate >= cutOffDate)
+                .OrderByDescending(sr => sr.SearchDate)
+                .ToListAsync();
+        }
+
+        public async Task<SearchResult> SaveSearchResultAsync(SearchResult result)
+        {
+            _context.SearchResults.Add(result);
+            await _context.SaveChangesAsync();
+            return result;
+        }
+    }
+}
