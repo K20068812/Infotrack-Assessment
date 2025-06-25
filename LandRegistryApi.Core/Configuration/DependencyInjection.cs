fbd5fb87c927dd24372520cbcead0de4b1feabea
Amman Kiani <amman.kiani@kcl.ac.uk>
2025-06-25 23:34:11 +0100

    add initial dependency registration

---
+++ LandRegistryApi.Core/Configuration/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using LandRegistryApi.Core.Interfaces;
+using LandRegistryApi.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LandRegistryApi.Core.Configuration
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCoreServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRankingService, RankingService>();
+            return services;
+        }
+    }
+}
---
+++ LandRegistryApi.Infrastructure/Configuration/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using LandRegistryApi.Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LandRegistryApi.Infrastructure.Configuration
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(OptionsBuilderExtensions.))
+
+            return services;
+        }
+    }
+}
