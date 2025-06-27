0ee566ae34887b59d7b0794827d101e690796e8b
Amman Kiani <amman.kiani@kcl.ac.uk>
2025-06-27 17:27:54 +0100

    play around with cookies

--- LandRegistryApi.Infrastructure/Services/GoogleSearchEngine.cs
+++ LandRegistryApi.Infrastructure/Services/GoogleSearchEngine.cs
@@ -1,10 +1,18 @@
-﻿using LandRegistryApi.Core.Interfaces;
+﻿using Azure.Core;
+using LandRegistryApi.Core.Interfaces;
+using System.Net;
 
 namespace LandRegistryApi.Infrastructure.Services
 {
     public class GoogleSearchEngine : ISearchEngine
     {
+        //todo: add logging?
         private readonly HttpClient _httpClient;
+
+        public GoogleSearchEngine(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
 
         public string BaseUrl => "https://www.google.co.uk/search?num=100&q=";
 
@@ -17,7 +25,18 @@ namespace LandRegistryApi.Infrastructure.Services
                 var encodedQuery = Uri.EscapeDataString(searchQuery);
                 var searchUrl = $"{BaseUrl}{encodedQuery}";
 
-                var response = await _httpClient.GetStringAsync(searchUrl);
+                var requestMessage = new HttpRequestMessage(HttpMethod.Get, searchUrl);
+
+                var cookieContainer = new CookieContainer();
+
+                var handler = new HttpClientHandler { CookieContainer = cookieContainer };
+                var client = new HttpClient(handler);
+
+                var response = await client.GetAsync("https://www.google.co.uk/search?num=100&q=land%20registry");
+                string content = await response.Content.ReadAsStringAsync();
+
+                //var response = await _httpClient.SendAsync(requestMessage);
+                //var body = await response.Content.ReadAsStringAsync();
 
                 return new List<int>(); // Todo: need to process the response
 
